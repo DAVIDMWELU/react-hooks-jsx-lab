295963e0259b7f8825b95b370229fdb5a495a99d
DAVIDMWELU <davidmwelu@gmail.com>
2025-02-18 10:32:31 +0300

    react hooks jsx

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,13 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        David is a Web Developer from Mombasa
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
